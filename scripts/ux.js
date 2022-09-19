e79ff9c499cc11e61f2a6a3d9f5faee7afa7a1c1
rwbahe <rwbahe@gmail.com>
2022-09-18 19:47:16 -0600

    responsive update-final

--- scripts/ux.js
+++ scripts/ux.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
 
     $("#nav-menu-button").click(function() {
         if ( $("#nav-dropdown-container").css("display") == "none" ) {
-            $("#nav-menu-button").css("background-image", "url(../../images/nav-menu-cancel-mobile.png)");
+            $("#nav-menu-button").css("background-image", "url(../images/nav-menu-cancel-mobile.png)");
             $("#nav-dropdown-container").css("display", "block");
         } else {
-            $("#nav-menu-button").css("background-image", "url(../../images/nav-menu-mobile.png)");
+            $("#nav-menu-button").css("background-image", "url(../images/nav-menu-mobile.png)");
             $("#nav-dropdown-container").css("display", "none");
         }
     })
