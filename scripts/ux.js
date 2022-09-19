95317d7e47457288153c1226ba99f6543ed0905d
rwbahe <rwbahe@gmail.com>
2022-09-18 19:57:51 -0600

    responsive update-final

--- scripts/ux.js
+++ scripts/ux.js
@@ -11,10 +11,12 @@ $(document).ready(function() {
 
     $("#nav-menu-button").click(function() {
         if ( $("#nav-dropdown-container").css("display") == "none" ) {
-            $("#nav-menu-button").css("background-image", "url(../images/nav-menu-cancel-mobile.png)");
+            //$("#nav-menu-button").css("background-image", "url(../images/nav-menu-cancel-mobile.png)");
+            $("#nav-menu-button").css("background-image", "url(https://rwbahe.github.io/images/nav-menu-cancel-mobile.png)");
             $("#nav-dropdown-container").css("display", "block");
         } else {
-            $("#nav-menu-button").css("background-image", "url(../images/nav-menu-mobile.png)");
+            //$("#nav-menu-button").css("background-image", "url(../images/nav-menu-mobile.png)");
+            $("#nav-menu-button").css("background-image", "url(https://rwbahe.github.io/images/nav-menu-mobile.png)");
             $("#nav-dropdown-container").css("display", "none");
         }
     })
